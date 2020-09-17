ffa01de5f8aceb107c53a0908dceafa2b34783e1
Danish <danishriz100@gmail.comgit commitexit>
2020-09-17 10:52:46 -0600

    hello world component created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HelloWorld from './components/HelloWorld';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <h1>React git Branch Merge Practice</h1>
+   <HelloWorld/>
+   <HelloWorld/>
+   <HelloWorld/>
    
+
   </React.StrictMode>,
   document.getElementById('root')
 );
