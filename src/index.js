42ade5cf43d4056023c6a980f69707b2f09e9327
Danish <danishriz100@gmail.comgit commitexit>
2020-09-17 11:11:59 -0600

    Hello student component created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HelloStudent from './components/HelloStudent';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-   
+    <h2>Git Branch Test</h2>
+   <HelloStudent name='danish'/>
+   <HelloStudent name='eboka'/>
   </React.StrictMode>,
   document.getElementById('root')
 );
